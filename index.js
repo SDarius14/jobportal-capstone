c143efa507338654385773b10555f364c5b09333
saul-worldfoodchain <sauldariush@gmail.com>
2023-07-29 20:08:54 +0800

    feat: Boiler plate

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const expressEjsLayouts = require("express-ejs-layouts");
+const expressLayouts = require("express-ejs-layouts");
+const app = express();
+
+app.use(expressEjsLayouts);
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.set("layout", "./layouts/master");
+
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
+
+app.listen(3300);
+console.log("listening to port: 3300");
