3a20530c9fbe7edb61338d81dc240e4531474be0
Jebson Ubaldo <ubaldojebson@gmail.com>
2023-07-31 18:08:15 +0800

    feat: added listing

--- index.js
+++ index.js
@@ -14,5 +14,21 @@ app.get("/", (req, res) => {
   res.render("pages/index");
 });
 
+app.get("/job-list", (req, res) => {
+  res.render("pages/job-list");
+});
+
+app.get("/job-details", (req, res) => {
+  res.render("pages/job-details");
+});
+
+app.get("/candidates", (req, res) => {
+  res.render("pages/candidates");
+});
+
+app.get("/candidate-profile", (req, res) => {
+  res.render("pages/candidate-profile");
+});
+
 app.listen(3300);
 console.log("listening to port: 3300");
